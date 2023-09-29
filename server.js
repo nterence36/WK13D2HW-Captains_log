741f36d9a16f4c104a8885ddec53d0a2d8611eda
Terence Nkeng <nterence36@gmail.com>
2023-09-29 18:03:45 -0400

    Create route added

--- server.js
+++ server.js
@@ -1,19 +1,31 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 
-const port = 3000
-const jsxEngine = require('jsx-view-engine')
+const port = 3000;
+const jsxEngine = require("jsx-view-engine");
 
-app.set('view engine', 'jsx')
-app.engine('jsx', jsxEngine())
+app.set("view engine", "jsx");
+app.engine("jsx", jsxEngine());
 
-app.use(express.urlencoded({extended:false}))
+// app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.render('New')
+
+app.get("/logs/new", (req, res) => {
+  res.render("New");
+});
+
+// Create route
+app.post("/logs", (req, res) => {
+  if (req.body.shipIsBroken === "on") {
+    req.body.shipIsBroken = true 
+  } else {
+    req.body.shipIsBroken = false 
+  }
+  res.send(req.body)
 })
-
 
 app.listen(port, () => {
-    console.log('Listening to port 3000')
-})
+  console.log("Listening to port 3000");
+});
--- views/New.jsx
+++ views/New.jsx
@@ -7,13 +7,13 @@ class New extends React.Component {
                 <h1>New Form</h1>
 
                 <form action='/logs' method='POST'>
-                    Title: <input type='text'/>
+                    Title: <input type='text' name='title'/>
                     <br/>
-                    Entry: <input type='textarea'/>
+                    Entry: <input type='textarea' name='entry'/>
                     <br/>
-                    shipIsBroken: <input type='checkbox'/>
+                    shipIsBroken: <input type='checkbox' name='shipIsBroken'/>
                     <br/>
-                    <input type="submit" name="" />
+                    <input type="submit"/>
                 </form>
             </div>
         )
